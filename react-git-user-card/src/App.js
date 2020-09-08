d22434f5d015e6f29002e7a9b67290dc4d80e231
Samantha Goodman <slgoodman89@gmail.com>
2020-09-08 15:06:34 -0400

    convert functional comp to class

--- react-git-user-card/src/App.js
+++ react-git-user-card/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+/* fetching data from : https://api.github.com/users/antashma 
+add '/followers' for followers 
+*/
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      userData: [], 
+    }
+  }
+
+  componentDidMount() {
+    console.log('sg: app.js : CDM engaged!')
+  }
+
+  render() {    
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>React Git User Fetcher App</h1>
+        <p>User data will display below.</p>
+        </header>
+        <section>
+          <p>User data...</p>
+        </section>
+      </div>
+    );
+  }
+
 }
 
 export default App;
